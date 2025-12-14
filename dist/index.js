8d229ddab3e9d3877f881f3174c1d56e39e8646e
Rino Miguel Sagario Quijote <rinomiguelq@gmail.com>
2025-12-14 16:55:25 +1100

    Updates to index.ts
    Added extra setup to TypeScript index.html, made index.ts attach eventlistener to main button

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hey");
-/*
-const appIdButton = document.getElementById("enterAppIdBtn");
+console.log("js file loaded");
+const appIdButton = document.getElementById("entferAppIdBtn");
 if (appIdButton != null) {
     console.log("added event");
     appIdButton.addEventListener("click", buttonClick);
 }
-
+else {
+    console.error("ERROR: Couldn't find App ID button.");
+}
 function buttonClick() {
-    const test = document.getElementById("appIdInput");
-    
-    if (test != null) {
-        console.log(test);
+    const appIdInput = document.getElementById("appIdInput").value;
+    if (appIdInput != null) {
+        console.log(appIdInput);
     }
 }
-*/ 
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-console.log("hey");
-/*
-const appIdButton = document.getElementById("enterAppIdBtn");
+console.log("js file loaded");
+
+const appIdButton = document.getElementById("entferAppIdBtn");
 if (appIdButton != null) {
     console.log("added event");
     appIdButton.addEventListener("click", buttonClick); 
+} else {
+    console.error("ERROR: Couldn't find App ID button.");
 }
 
 function buttonClick() {
-    const test = document.getElementById("appIdInput");
-    
-    if (test != null) {
-        console.log(test);
+    const appIdInput = (<HTMLInputElement>document.getElementById("appIdInput")).value;
+    if (appIdInput != null) {
+        console.log(appIdInput);
     }
 }
-*/
