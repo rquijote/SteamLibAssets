2436f61de841e5f202dde4cfab58c09574f394af
Rino Miguel Sagario Quijote <rinomiguelq@gmail.com>
2025-12-14 15:06:29 +1100

    Added and Setup TypeScript, Added initial index, stylesheet, index.ts

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("hey");
+/*
+const appIdButton = document.getElementById("enterAppIdBtn");
+if (appIdButton != null) {
+    console.log("added event");
+    appIdButton.addEventListener("click", buttonClick);
+}
+
+function buttonClick() {
+    const test = document.getElementById("appIdInput");
+    
+    if (test != null) {
+        console.log(test);
+    }
+}
+*/ 
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+console.log("hey");
+/*
+const appIdButton = document.getElementById("enterAppIdBtn");
+if (appIdButton != null) {
+    console.log("added event");
+    appIdButton.addEventListener("click", buttonClick); 
+}
+
+function buttonClick() {
+    const test = document.getElementById("appIdInput");
+    
+    if (test != null) {
+        console.log(test);
+    }
+}
+*/
