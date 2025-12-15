96b4e3dceac2a7912bb451353ce6e5e750af05ee
Rino Miguel Sagario Quijote <rinomiguelq@gmail.com>
2025-12-15 14:29:41 +1100

    Added vite. Started on downloadBtn implementation.
    Added vite to fix importing and for the dev server. Hoping it adds simplicity to the project instead of fluff. Added jszip to create a zip file to download images later. Created imgIds in printAllAssets function to give them unique names in the future.

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+import JSZip from "jszip";
 //Shift f1 -> Run Build Task to run TS watch
 console.log("js file loaded");
 /**
@@ -6,11 +7,18 @@ console.log("js file loaded");
  *
  */
 const appIdButton = document.getElementById("enterAppIdBtn");
+const downloadBtn = document.getElementById("downloadBtn");
 if (appIdButton == null) {
     console.error("Couldn't find App ID button.");
 }
 else {
     appIdButton.addEventListener("click", enterAppIDButtonClick);
+}
+if (downloadBtn == null) {
+    console.error("Couldn't find App ID button.");
+}
+else {
+    downloadBtn.addEventListener("click", downloadButtonClick);
 }
 async function enterAppIDButtonClick() {
     const appIdInputValue = (document.getElementById("appIdInput")).value;
@@ -18,6 +26,27 @@ async function enterAppIDButtonClick() {
         return;
     const blobs = await getAllAssets(appIdInputValue);
     printAllAssets(blobs);
+}
+async function downloadButtonClick() {
+    const imgs = document.querySelectorAll("img"); // Selects all imgs
+    let urls = [];
+    imgs.forEach((el) => {
+        if (el.nextElementSibling.checked) {
+            // Presumes all next element of img are an HTMLInputElement.
+            urls.push(el.src);
+        }
+    });
+    console.log(urls);
+    const promises = urls.map(async (url) => {
+        const result = await fetch(url);
+        const blob = await result.blob();
+        return blob;
+    });
+    const blobs = await Promise.all(promises);
+    const zip = new JSZip();
+    blobs.forEach((blob) => {
+        zip.file("image.jpg", blob);
+    });
 }
 /**
  *
@@ -31,13 +60,17 @@ async function getAllAssets(appID) {
         `https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/${appID}/header.jpg`, // header
         `https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/${appID}/library_600x900.jpg`, // library capsule
         `https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/${appID}/library_600x900_2x.jpg`, // library capsule 2x
-        `https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/${appID}/library_hero.jpg`, // library hero
+        `hfttps://shared.fastly.steamstatic.com/store_item_assets/steam/apps/${appID}/library_hero.jpg`, // library hero
     ];
     let blobs = [];
     for (const url of urls) {
         const blob = await getBlob(url);
-        if (blob)
+        if (blob) {
             blobs.push(blob);
+        }
+        else {
+            blobs.push(undefined);
+        }
     }
     return blobs;
 }
@@ -61,11 +94,33 @@ async function getBlob(url) {
  *
  */
 function printAllAssets(blobs) {
-    blobs.forEach((blob) => {
+    const imgIds = [
+        "main_capsule_img",
+        "small_capsule_img",
+        "header_img",
+        "library_capsule_img",
+        "2xlibrary_capsule_img",
+        "library_hero_img",
+    ];
+    for (let i = 0; i < blobs.length; i++) {
+        let div = document.createElement("div");
         let img = document.createElement("img");
-        img.src = URL.createObjectURL(blob);
-        document.body.appendChild(img);
-    });
+        let input = document.createElement("input");
+        input.type = "checkbox";
+        const blob = blobs[i];
+        if (!blob) {
+            img.src =
+                "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg";
+            input.checked = false;
+        }
+        else {
+            img.src = URL.createObjectURL(blob);
+            input.checked = true;
+        }
+        img.id = imgIds[i];
+        div.appendChild(img);
+        div.appendChild(input);
+        document.body.appendChild(div);
+    }
 }
-export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
+import JSZip from "jszip";
+
 //Shift f1 -> Run Build Task to run TS watch
 console.log("js file loaded");
 
-/**  
- * 
+/**
+ *
  * Event Listeners
- * 
+ *
  */
 
 const appIdButton = document.getElementById("enterAppIdBtn");
+const downloadBtn = document.getElementById("downloadBtn");
 if (appIdButton == null) {
   console.error("Couldn't find App ID button.");
 } else {
-    appIdButton.addEventListener("click", enterAppIDButtonClick); 
+  appIdButton.addEventListener("click", enterAppIDButtonClick);
+}
+
+if (downloadBtn == null) {
+  console.error("Couldn't find App ID button.");
+} else {
+  downloadBtn.addEventListener("click", downloadButtonClick);
 }
 
 async function enterAppIDButtonClick() {
@@ -23,10 +32,34 @@ async function enterAppIDButtonClick() {
   printAllAssets(blobs);
 }
 
-/** 
- * 
- * Fetch Assets Functions 
- * 
+async function downloadButtonClick() {
+  const imgs = document.querySelectorAll("img"); // Selects all imgs
+  let urls: string[] = [];
+  imgs.forEach((el) => {
+    if ((el.nextElementSibling! as HTMLInputElement).checked) {
+      // Presumes all next element of img are an HTMLInputElement.
+      urls.push(el.src);
+    }
+  });
+  console.log(urls);
+
+  const promises = urls.map(async (url) => {
+    const result = await fetch(url);
+    const blob = await result.blob();
+    return blob;
+  });
+
+  const blobs = await Promise.all(promises);
+  const zip = new JSZip();
+  blobs.forEach((blob) => {
+    zip.file("image.jpg", blob);
+  });
+}
+
+/**
+ *
+ * Fetch Assets Functions
+ *
  */
 
 async function getAllAssets(appID: string) {
@@ -36,12 +69,16 @@ async function getAllAssets(appID: string) {
     `https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/${appID}/header.jpg`, // header
     `https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/${appID}/library_600x900.jpg`, // library capsule
     `https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/${appID}/library_600x900_2x.jpg`, // library capsule 2x
-    `https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/${appID}/library_hero.jpg`, // library hero
+    `hfttps://shared.fastly.steamstatic.com/store_item_assets/steam/apps/${appID}/library_hero.jpg`, // library hero
   ];
-  let blobs: Blob[] = [];
+  let blobs: (Blob | undefined)[] = [];
   for (const url of urls) {
     const blob = await getBlob(url);
-    if (blob) blobs.push(blob);
+    if (blob) {
+      blobs.push(blob);
+    } else {
+      blobs.push(undefined);
+    }
   }
 
   return blobs;
@@ -61,16 +98,39 @@ async function getBlob(url: string) {
   }
 }
 
-/** 
- * 
+/**
+ *
  * Frontend DOM Functions
- * 
+ *
  */
 
-function printAllAssets(blobs: Blob[]) {
-  blobs.forEach((blob) => {
+function printAllAssets(blobs: (Blob | undefined)[]) {
+  const imgIds : string[] = [
+    "main_capsule_img",
+    "small_capsule_img",
+    "header_img",
+    "library_capsule_img",
+    "2xlibrary_capsule_img",
+    "library_hero_img",
+  ];
+
+  for (let i = 0; i < blobs.length; i++) {
+    let div = document.createElement("div");
     let img = document.createElement("img");
-    img.src = URL.createObjectURL(blob);
-    document.body.appendChild(img);
-  });
+    let input = document.createElement("input");
+    input.type = "checkbox";
+    const blob = blobs[i];
+    if (!blob) {
+      img.src =
+        "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg";
+      input.checked = false;
+    } else {
+      img.src = URL.createObjectURL(blob);
+      input.checked = true;
+    }
+    img.id = imgIds[i]!;
+    div.appendChild(img);
+    div.appendChild(input);
+    document.body.appendChild(div);
+  }
 }
