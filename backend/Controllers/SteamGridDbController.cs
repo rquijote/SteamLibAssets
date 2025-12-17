c12ad0e22ff098e2791db9bb91c437d743c31f25
Rino Miguel Sagario Quijote <rinomiguelq@gmail.com>
2025-12-18 10:30:02 +1100

    Update SteamGridDbController.cs
    Added get heroes, logos, and icons to SteamGridDbController.cs

--- backend/Controllers/SteamGridDbController.cs
+++ backend/Controllers/SteamGridDbController.cs
@@ -15,5 +15,32 @@ namespace SteamLibAssets.Controllers
             SteamGridDbGrid[]? grids = await sgdb.GetGridsByGameIdAsync(appId);
             return Ok(grids);
         }
+
+        [HttpGet("heroes/{appId}")]
+        public async Task<IActionResult> GetHeroes(int appId)
+        {
+            var apiKey = Environment.GetEnvironmentVariable("STEAMGRIDDB_API_KEY");
+            SteamGridDb sgdb = new SteamGridDb(apiKey);
+            SteamGridDbHero[]? heroes = await sgdb.GetHeroesByGameIdAsync(appId);
+            return Ok(heroes);
+        }
+
+        [HttpGet("logos/{appId}")]
+        public async Task<IActionResult> GetLogos(int appId)
+        {
+            var apiKey = Environment.GetEnvironmentVariable("STEAMGRIDDB_API_KEY");
+            SteamGridDb sgdb = new SteamGridDb(apiKey);
+            SteamGridDbLogo[]? logos = await sgdb.GetLogosByGameIdAsync(appId);
+            return Ok(logos);
+        }
+
+        [HttpGet("icons/{appId}")]
+        public async Task<IActionResult> GetIcons(int appId)
+        {
+            var apiKey = Environment.GetEnvironmentVariable("STEAMGRIDDB_API_KEY");
+            SteamGridDb sgdb = new SteamGridDb(apiKey);
+            SteamGridDbIcon[]? icons = await sgdb.GetIconsByGameIdAsync(appId);
+            return Ok(icons);
+        }
     }
 }
