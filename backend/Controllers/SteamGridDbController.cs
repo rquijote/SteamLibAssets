5956c05133c86852b6d65de497ba8086029ab5fa
Rino Miguel Sagario Quijote <rinomiguelq@gmail.com>
2025-12-20 14:14:39 +1100

    Update SteamGridDbController.cs
    Added pagination on the controller. Still needs to be implemented onto the front end.

--- backend/Controllers/SteamGridDbController.cs
+++ backend/Controllers/SteamGridDbController.cs
@@ -12,11 +12,30 @@ namespace SteamLibAssets.Controllers
     {
         private readonly SteamGridDbService _sgdbService = new SteamGridDbService();
 
+        private IActionResult PaginateAssets(List<Asset> assets, int page, int pageSize)
+        {
+            var totalItems = assets.Count;
+            var totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+
+            var pagedAssets = assets
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            return Ok(new
+            {
+                Page = page,
+                PageSize = pageSize,
+                TotalItems = totalItems,
+                TotalPages = totalPages,
+                Items = pagedAssets
+            });
+        }
+
         [HttpGet("grids/{appId}")]
-        public async Task<IActionResult> GetGrids(int appId)
+        public async Task<IActionResult> GetGrids(int appId, int page = 1, int pageSize = 10)
         {
             var grids = await _sgdbService.GetGridsAsync(appId);
-
             var assets = AssetMapper.MapAssets(
                 grids,
                 x => x.FullImageUrl,
@@ -26,11 +45,11 @@ namespace SteamLibAssets.Controllers
                 x => x.Author
             );
 
-            return Ok(assets);
+            return PaginateAssets(assets, page, pageSize);
         }
 
         [HttpGet("heroes/{appId}")]
-        public async Task<IActionResult> GetHeroes(int appId)
+        public async Task<IActionResult> GetHeroes(int appId, int page = 1, int pageSize = 10)
         {
             var heroes = await _sgdbService.GetHeroesAsync(appId);
             var assets = AssetMapper.MapAssets(
@@ -41,11 +60,12 @@ namespace SteamLibAssets.Controllers
                 x => x.Height,
                 x => x.Author
             );
-            return Ok(assets);
+
+            return PaginateAssets(assets, page, pageSize);
         }
 
         [HttpGet("logos/{appId}")]
-        public async Task<IActionResult> GetLogos(int appId)
+        public async Task<IActionResult> GetLogos(int appId, int page = 1, int pageSize = 10)
         {
             var logos = await _sgdbService.GetLogosAsync(appId);
             var assets = AssetMapper.MapAssets(
@@ -56,11 +76,12 @@ namespace SteamLibAssets.Controllers
                 x => x.Height,
                 x => x.Author
             );
-            return Ok(assets);
+
+            return PaginateAssets(assets, page, pageSize);
         }
 
         [HttpGet("icons/{appId}")]
-        public async Task<IActionResult> GetIcons(int appId)
+        public async Task<IActionResult> GetIcons(int appId, int page = 1, int pageSize = 10)
         {
             var icons = await _sgdbService.GetIconsAsync(appId);
             var assets = AssetMapper.MapAssets(
@@ -71,7 +92,8 @@ namespace SteamLibAssets.Controllers
                 x => x.Height,
                 x => x.Author
             );
-            return Ok(assets);
+
+            return PaginateAssets(assets, page, pageSize);
         }
     }
 }
